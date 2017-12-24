15162481e8edf65633f6b15998c9e5001f955db2
harada2 <createdirect2@gmail.com>
2017-12-25 00:37:32 +0900

    初回プッシュ

---
+++ TestCodeProject/Assets/SceneChanger/Script/ISceneChanger.cs
@@ -0,0 +1,19 @@
+﻿//**********************************************************
+//ISceneChanger.cs
+//Author harada
+//**********************************************************
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//**********************************************************
+//ISceneChangerInterFace
+//シーンチェンジ関数
+//**********************************************************
+public interface ISceneChanger
+{
+  void ChangeScene(string sceneName);//シーン切り替え関数
+  void BackScene();//バック用関数
+
+  bool IsDone { get; }//シーンチェンジ完了かどうかのフラグ
+}
---
+++ TestCodeProject/Assets/SceneChanger/Script/ISceneChangerAnimation.cs
@@ -0,0 +1,18 @@
+﻿//**********************************************************
+//ISceneChangerAnimation.cs
+//Author harada
+//**********************************************************
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//**********************************************************
+//ISceneChangerAnimationInterFace
+//シーンチェンジアニメーション（フェード等）
+//**********************************************************
+public interface ISceneChangerAnimation
+{
+  void PlayInAnimation();
+  void PlayOutAnimation();
+  bool IsDone { get; }
+}
